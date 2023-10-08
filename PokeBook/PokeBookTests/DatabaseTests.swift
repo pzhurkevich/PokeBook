357659b6cabe0f3027ac33d99640d570ecc6ff56
Pavel Zh <pzworkmail@gmail.com>
2023-10-08 18:00:08 +0300

    FIXED database test

--- PokeBook/PokeBookTests/DatabaseTests.swift
+++ PokeBook/PokeBookTests/DatabaseTests.swift
@@ -4,5 +4,22 @@
 //
 //  Created by Pavel on 8.10.23.
 //
+import XCTest
+import RealmSwift
+@testable import PokeBook
 
-import Foundation
+class RealmManagerTests: XCTestCase {
+
+
+    // MARK: - Test addPokemonListData
+    
+    func testAddPokemonListData() {
+        let testData = Pokemon(name: "testPokemon", pokemonURL: "testURL")
+        RealmManager.shared.addPokemonListData(data: testData)
+        let savedPokemons = RealmManager.shared.fetchAllSavedPokemons()
+        XCTAssertFalse(savedPokemons.isEmpty, "Database with test data cannot be empty")
+        XCTAssertTrue(savedPokemons.contains(where: {$0.name == testData.name}))
+        XCTAssertTrue(savedPokemons.contains(where: {$0.pokemonURL == testData.pokemonURL}))
+        XCTAssertEqual(savedPokemons.filter({$0.pokemonURL == testData.pokemonURL}).count, 1, "test data should only be in one instance")
+    }
+}
